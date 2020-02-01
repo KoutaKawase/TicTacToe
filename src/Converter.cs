926504af4236bab6b38003644bbbe4e87c37a73b
KoutaKawase <suhrkawase@gmail.com>
2020-02-01 20:24:14 +0900

    ♻️  boardの文字列変換処理をメソッドに切り分けた
    見やすくするため

--- src/Converter.cs
+++ src/Converter.cs
@@ -7,29 +7,68 @@ namespace TicTacToe
 {
     public class Converter
     {
-        public static string ConvertToStringFrom(IList<MarkType> state)
+        private static IEnumerable<IEnumerable<MarkType>> CreateChunksFrom(IList<MarkType> state, int chunkSize)
         {
-            var boardBuilder = new StringBuilder("  +---+---+---+\n");
-            var chunkSize = 3;
-            var chunks = state.Select((value, index) => new { value, index })
+            return state.Select((value, index) => new { value, index })
                 .GroupBy(x => x.index / chunkSize)
                 .Select(g => g.Select(x => x.value));
+        }
 
-            foreach (var chunk in chunks.Select((value, index) => new { Value = value, Index = index }))
+        private static StringBuilder DiscernMark(MarkType mark, StringBuilder sb)
+        {
+            switch (mark)
             {
-                boardBuilder.Append($"{chunk.Index + 1} |");
-                foreach (var item in chunk.Value)
-                {
-                    switch (item)
-                    {
-                        case MarkType.Circle:
-                            boardBuilder.Append(" @ |");
-                            break;
-                    }
-                }
-                boardBuilder.Append("\n  +---+---+---+\n");
+                case MarkType.Circle:
+                    sb.Append(" @ |");
+                    break;
+                case MarkType.Cross:
+                    sb.Append(" X |");
+                    break;
+                case MarkType.None:
+                    sb.Append("   |");
+                    break;
             }
-            boardBuilder.Append("    A   B   C  ");
+
+            return sb;
+        }
+        private static StringBuilder BuildMarkAndDecoration(StringBuilder sb, IEnumerable<MarkType> marks)
+        {
+            foreach (var mark in marks)
+            {
+                sb = DiscernMark(mark, sb);
+            }
+            //行の区切り文字を生成
+            sb.Append("\n  +---+---+---+\n");
+
+            return sb;
+        }
+
+        private static StringBuilder buildBoard(StringBuilder sb, IEnumerable<IEnumerable<MarkType>> chunks)
+        {
+            //上辺を描写
+            sb.Append("  +---+---+---+\n");
+
+            foreach (var chunk in chunks.Select((marks, index) => new { Marks = marks, Index = index }))
+            {
+                //COLUMN位置目印を生成
+                sb.Append($"{chunk.Index + 1} |");
+                //ボード本体生成
+                sb = BuildMarkAndDecoration(sb, chunk.Marks);
+            }
+            //ROW位置目印を生成
+            sb.Append("    A   B   C  ");
+
+            return sb;
+        }
+        public static string ConvertToStringFrom(IList<MarkType> state)
+        {
+            var boardBuilder = new StringBuilder();
+            //stateを３個ずつに分けて配列にする
+            var chunkSize = 3;
+            var chunks = CreateChunksFrom(state, chunkSize);
+
+            //ボードの文字列表現生成
+            boardBuilder = buildBoard(boardBuilder, chunks);
 
             return boardBuilder.ToString();
         }
